60475e6c74e17b74bd941e35d8a31579e8b6ee54
koreavc <koreavc@DESKTOP-VFUB301>
2023-07-27 15:21:18 +0900

    test

---
+++ aDI/src/ex1_xml2_ref/MemberDAO.java
@@ -0,0 +1,23 @@
+package ex1_xml2_ref;
+
+public class MemberDAO {
+		private MemberBean member;
+		
+		public MemberDAO() {
+			// TODO Auto-generated constructor stub
+		
+		}
+		public MemberDAO(MemberBean member) {
+			this.member=member;
+		}
+		public MemberBean getMember() {
+			return member;
+		}
+		public void setMember(MemberBean member) {
+			this.member = member;
+		}
+		public void insert() {
+			member.output();
+			System.out.println("디비 입력!");
+		}
+}
